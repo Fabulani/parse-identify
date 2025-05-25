521ad1dd7513ae08959b5cadd0137ad509a7dade
Fabiano Manschein <16107182+Fabulani@users.noreply.github.com>
2025-05-25 05:01:46 +0300

    Use clangd for automatic formatting (#3)

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,29 @@
-#include <iostream>
-#include <string>
-#include <array>
-
 #include "parser.h"
 #include "pprinter.h"
 
-int main(int argc, char *argv[])
-{
-	// Check if the user provided an argument.
-	if (argc != 2 || std::string(argv[1]) == "--help" || std::string(argv[1]) == "-h")
-	{
-		const std::string error_message = "Usage: " + std::string(argv[0]) + " file_path\n";
-		std::cerr << error_message;
-		return 1;
-	}
+#include <array>
+#include <iostream>
+#include <string>
 
-	std::string file_path = argv[1];
-	std::array<char, ATA_IDENTIFY_SIZE> file_buffer = read_file_to_buffer(file_path);
+int main(int argc, char *argv[]) {
+  // Check if the user provided an argument.
+  if (argc != 2 || std::string(argv[1]) == "--help" ||
+      std::string(argv[1]) == "-h") {
+    const std::string error_message =
+        "Usage: " + std::string(argv[0]) + " file_path\n";
+    std::cerr << error_message;
+    return 1;
+  }
 
-	// Parse the file buffer
-	std::string model_number = extract_model_number(file_buffer);
-	unsigned int highest_dma_mode = extract_dma_mode(file_buffer);
-	bool smart_supported = extract_smart_support(file_buffer);
+  std::string file_path = argv[1];
+  std::array<char, ATA_IDENTIFY_SIZE> file_buffer =
+      read_file_to_buffer(file_path);
 
-	print_results(file_path, model_number, highest_dma_mode, smart_supported);
-	return 0;
+  // Parse the file buffer
+  std::string model_number = extract_model_number(file_buffer);
+  unsigned int highest_dma_mode = extract_dma_mode(file_buffer);
+  bool smart_supported = extract_smart_support(file_buffer);
+
+  print_results(file_path, model_number, highest_dma_mode, smart_supported);
+  return 0;
 }
--- src/parser.h
+++ src/parser.h
@@ -1,15 +1,19 @@
 #pragma once
-#include <string>
+#include <algorithm>
 #include <array>
 #include <fstream>
-#include <algorithm>
+#include <string>
 
-constexpr std::size_t ATA_IDENTIFY_SIZE = 512; // ATA IDENTIFY data structure size in bytes
+constexpr std::size_t ATA_IDENTIFY_SIZE =
+    512; // ATA IDENTIFY data structure size in bytes
 
-// Reads a file into a buffer. Checks if the file is binary, has 512 bytes, and is opened successfully.
-std::array<char, ATA_IDENTIFY_SIZE> read_file_to_buffer(const std::string &file_path);
+// Reads a file into a buffer. Checks if the file is binary, has 512 bytes, and
+// is opened successfully.
+std::array<char, ATA_IDENTIFY_SIZE>
+read_file_to_buffer(const std::string &file_path);
 
 // Parser functions for extracting specific information from the file buffer.
-std::string extract_model_number(std::array<char, ATA_IDENTIFY_SIZE> &file_buffer);
+std::string
+extract_model_number(std::array<char, ATA_IDENTIFY_SIZE> &file_buffer);
 unsigned int extract_dma_mode(std::array<char, ATA_IDENTIFY_SIZE> &file_buffer);
 bool extract_smart_support(std::array<char, ATA_IDENTIFY_SIZE> &file_buffer);
